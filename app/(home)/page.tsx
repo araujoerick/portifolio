feb809f2322b0bc4b40c5db255a11727a6dff455
Erick Araújo <araujoerick.dev@gmail.com>
2025-01-10 01:30:21 -0300

    🔧 chore: setup Poppins font

---
+++ app/(home)/page.tsx
@@ -0,0 +1,14 @@
+import Header from "../_components/header";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <main>
+        <h1 className="text-4xl">
+          Olá, meu nome é Erick Araújo <span>Desenvolvedor Front-End</span>
+        </h1>
+      </main>
+    </>
+  );
+}
