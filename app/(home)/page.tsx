4e028932ea1da6aa8f53ddaa5a91ac472c6de529
Erick Araújo <araujoerick.dev@gmail.com>
2025-01-17 17:28:09 -0300

    ✨ feat: add background with tsparticles on Home

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,14 +1,20 @@
-import Header from "../_components/header";
+import ParticlesBackground from "../_components/particles-background";
 
 export default function Home() {
   return (
     <>
-      <Header />
-      <main>
-        <h1 className="text-4xl">
-          Olá, meu nome é Erick Araújo{" "}
-          <span className="text-pear">Desenvolvedor Front-End</span>
-        </h1>
+      <main className="relative flex h-screen flex-col items-center justify-center">
+        <div className="fixed left-1/2 top-0 w-full -translate-x-1/2">
+          <ParticlesBackground />
+        </div>
+        <div className="px-8">
+          <h1 className="max-w-[30ch] space-y-2 text-center text-5xl text-neutral-700">
+            <p>Olá, meu nome é Erick Araújo</p>
+            <p className="text-pear-600 mix-blend-difference">
+              Desenvolvedor Front-End
+            </p>
+          </h1>
+        </div>
       </main>
     </>
   );
---
+++ app/_components/particles-background.tsx
@@ -0,0 +1,102 @@
+"use client";
+import { useEffect, useMemo, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import type { Engine, ISourceOptions } from "@tsparticles/engine";
+
+import { loadFull } from "tsparticles";
+
+const ParticlesBackground = () => {
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    initParticlesEngine(async (engine: Engine) => {
+      await loadFull(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesOptions: ISourceOptions = useMemo(
+    () => ({
+      background: {
+        color: {
+          value: "bg-background",
+        },
+      },
+      style: {
+        position: "relative",
+        height: "100vh",
+      },
+      fpsLimit: 75,
+      interactivity: {
+        events: {
+          onClick: {
+            enable: false,
+            mode: "push",
+          },
+          onHover: {
+            enable: true,
+            mode: "repulse",
+          },
+          resize: {
+            enable: true,
+          },
+        },
+        modes: {
+          push: {
+            quantity: 4,
+          },
+          repulse: {
+            distance: 200,
+            duration: 0.4,
+          },
+        },
+      },
+      particles: {
+        color: {
+          value: "#B7E91B",
+        },
+        links: {
+          color: "#B7E91B",
+          distance: 150,
+          enable: true,
+          opacity: 0.5,
+          width: 1,
+        },
+        move: {
+          direction: "none",
+          enable: true,
+          outModes: {
+            default: "bounce",
+          },
+          random: false,
+          speed: 3,
+          straight: false,
+        },
+        number: {
+          density: {
+            enable: true,
+            width: 1100,
+            height: 1000,
+          },
+          value: 40,
+        },
+        opacity: {
+          value: 0.8,
+        },
+        shape: {
+          type: "circle",
+        },
+        size: {
+          value: { min: 1, max: 5 },
+        },
+      },
+      detectRetina: true,
+    }),
+    [],
+  );
+
+  return <>{init && <Particles options={particlesOptions} />}</>;
+};
+
+export default ParticlesBackground;
