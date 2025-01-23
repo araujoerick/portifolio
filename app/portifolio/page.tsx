26ee0b976415503c438738e77d2206870afafb4e
Erick Araújo <araujoerick.dev@gmail.com>
2025-01-23 01:29:43 -0300

    ✨ feat: add initial portifolio page

--- app/portifolio/page.tsx
+++ app/portifolio/page.tsx
@@ -1,5 +1,23 @@
 const PortifolioPage = () => {
-  return <section className="mt-28">PortifolioPage</section>;
+  return (
+    <section className="flex justify-center">
+      <div className="container space-y-8 p-8">
+        <div className="space-y-4">
+          <h1 className="text-5xl font-extralight">Tecnologias</h1>
+          <p className="font-light">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
+            ipsum ad in veniam minus vitae tempora totam, aliquam ullam
+            doloribus? Nesciunt, excepturi officiis. Laboriosam, ducimus
+            inventore quam temporibus fugit dolor.
+          </p>
+        </div>
+
+        <div className="space-y-4">
+          <h1 className="text-5xl font-extralight">Projetos</h1>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default PortifolioPage;
