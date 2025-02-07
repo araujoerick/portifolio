50dc9d1983f137d7ff8dfa4288840b36a2cea8de
Erick Araújo <araujoerick.dev@gmail.com>
2025-02-06 21:20:16 -0300

    ✨ feat: add props to motion div component

--- app/_components/ui/enter-animation.tsx
+++ app/_components/ui/enter-animation.tsx
@@ -1,17 +1,21 @@
 "use client";
 
-import { motion } from "framer-motion";
+import { motion, MotionProps } from "framer-motion";
 import { PropsWithChildren } from "react";
 
-const EnterAnimation = ({ children }: PropsWithChildren) => {
+type EnterAnimationProps = PropsWithChildren<MotionProps> &
+  React.HTMLAttributes<HTMLDivElement>;
+
+const EnterAnimation = ({ children, ...props }: EnterAnimationProps) => {
   return (
-    <motion.section
+    <motion.div
+      {...props}
       initial={{ opacity: 0, x: -100 }}
       whileInView={{ opacity: 1, x: 0 }}
       transition={{ duration: 0.5, delay: 0.3 }}
     >
       {children}
-    </motion.section>
+    </motion.div>
   );
 };
 
