619d419af371ce80f241b1936541b01324ee8c0e
araujoerick <araujoerick.dev@gmail.com>
2025-02-09 20:56:14 -0300

    ✨ feat: ensure ButtonDownloadCv receive custom classnames

--- app/_components/ui/button-cv.tsx
+++ app/_components/ui/button-cv.tsx
@@ -1,14 +1,24 @@
 import Link from "next/link";
-import React from "react";
+
+import { cn } from "@/app/_lib/utils";
 
 import { Button, ButtonProps } from "./button";
 
+interface ButtonDownloadCvProps extends ButtonProps {
+  className?: string;
+}
+
 const ButtonDownloadCv = ({
   children,
+  className,
   ...props
-}: React.PropsWithChildren<ButtonProps>) => {
+}: React.PropsWithChildren<ButtonDownloadCvProps>) => {
   return (
-    <Button {...props} className="bg-inherit p-2 text-xs sm:text-sm" asChild>
+    <Button
+      {...props}
+      className={cn("p-2 text-xs sm:text-sm", className)}
+      asChild
+    >
       <Link
         href="/docs/araujoerick-curriculo.pdf"
         target="_blank"
